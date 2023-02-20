01832ff641a5e0eda85d1e5d8bdddbc4d9e0c75f
Ted Merchant <ted.merchant@corp.cision.com>
2023-02-20 09:09:52 -0600

    Attempt to build docker image 2

--- .github/actions/docker/docker-build.sh
+++ .github/actions/docker/docker-build.sh
@@ -2,5 +2,9 @@
 
 set -e
 
+SERVICE_NAME=${SERVICE_NAME?:SERVICE_NAME apollo-router}
+VERSION=${VERSION?:VERSION http2-temp-fix}
 
-docker build -f ${DOCKER_RESOURCES}/Dockerfile.router -t "http2-temp-fix" \
+IMAGE_NAME="$(echo "${SERVICE_NAME}:${VERSION}" | tr '[:upper:]' '[:lower:]')"
+
+docker build -f ${DOCKER_RESOURCES}/Dockerfile -t "$IMAGE_NAME" \
