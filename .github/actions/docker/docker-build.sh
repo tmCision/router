3c3282c3241e91bbe703efbbee6e5bb46170a72d
Ted Merchant <ted.merchant@corp.cision.com>
2023-02-20 09:51:36 -0600

    Hardcode image name

--- .github/actions/docker/docker-build.sh
+++ .github/actions/docker/docker-build.sh
@@ -5,6 +5,6 @@ set -e
 SERVICE_NAME=${SERVICE_NAME?:SERVICE_NAME apollo-router}
 VERSION=${VERSION?:VERSION http2-temp-fix}
 
-IMAGE_NAME="$(echo "${SERVICE_NAME}:${VERSION}" | tr '[:upper:]' '[:lower:]')"
-
+#IMAGE_NAME="$(echo "${SERVICE_NAME}:${VERSION}" | tr '[:upper:]' '[:lower:]')"
+IMAGE_NAME="APOLLO-ROUTER:http2-disabled"
 docker build -f ${DOCKER_RESOURCES}/Dockerfile -t "$IMAGE_NAME" \
